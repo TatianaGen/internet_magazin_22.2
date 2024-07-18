b4ca28c2d113d33691b0d7e52fb4fec1536c5d71
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-07-18 21:06:46 +0300

    21.1

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,13 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from catalog.apps import CatalogConfig
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, ProductDeleteView
+
+app_name = CatalogConfig.name
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("catalog.urls", namespace="catalog")),
-    path('blog/', include('blog.urls', namespace='blog'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', ProductListView.as_view(), name='product_list'),
+    path('catalog/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
+    path('catalog/create', ProductCreateView.as_view(), name='product_form'),
+    path('catalog/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
+    path('catalog/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
+]
