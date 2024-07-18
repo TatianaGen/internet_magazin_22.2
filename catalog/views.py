8b620bfa41ebcbba453cfcfbab67b5414dad2be0
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-07-18 21:09:51 +0300

    21.1

--- catalog/views.py
+++ catalog/views.py
@@ -1,46 +1,39 @@
+from django.shortcuts import render, get_object_or_404
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from django.shortcuts import render, get_object_or_404
+
 from catalog.models import Product
-
-
-def home(request):
-    return render(request, "home.html")
-
-
-def contacts(request):
-    return render(request, "contacts.html")
 
 
 class ProductListView(ListView):
     model = Product
 
-
-class ProductDetailView(DetailView):
-    model = Product
-
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
-        self.object.views_counter += 1
+        self.object.views_count += 1
         self.object.save()
         return self.object
 
 
+class ProductDetailView(DetailView):
+    model = Product
+
+
 class ProductCreateView(CreateView):
     model = Product
-    fields = '__all__'  # передаем все поля которые есть в классе Product
-    success_url = reverse_lazy('catalog:products_list')
+    fields = ('name', 'product_description', 'product_photo', 'category', 'price', 'created_at', 'updated_at')
+    success_url = reverse_lazy('catalog:product_list')
 
 
 class ProductUpdateView(UpdateView):
     model = Product
-    fields = '__all__'
-    success_url = reverse_lazy('catalog:products_list')
+    fields = ('name', 'product_description', 'product_photo', 'category', 'price', 'created_at', 'updated_at')
+    success_url = reverse_lazy('catalog:product_list')
 
     def get_success_url(self):
-        return reverse('catalog:products_detail', args=[self.kwargs.get('pk')])
+        return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
 
 
 class ProductDeleteView(DeleteView):
     model = Product
-    success_url = reverse_lazy('catalog:products_list')
+    success_url = reverse_lazy('catalog:product_list')
