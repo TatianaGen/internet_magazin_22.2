856257df00f2046ee65e32284ff1e042fa5100ac
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-07-22 20:40:01 +0300

    22.1

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+f
 from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
