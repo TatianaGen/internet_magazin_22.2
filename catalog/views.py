3ab1afde41e23f362f04da30f0faa380f3da0408
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-07-29 21:17:13 +0300

    22.2

--- catalog/views.py
+++ catalog/views.py
@@ -2,37 +2,25 @@ from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from catalog.models import Product
-
-
 class ProductListView(ListView):
     model = Product
-
     def get_object(self, queryset=None):
         self.object = super().get_object(queryset)
         self.object.views_count += 1
         self.object.save()
         return self.object
-
-
 class ProductDetailView(DetailView):
     model = Product
-
-
 class ProductCreateView(CreateView):
     model = Product
     fields = ('name', 'product_description', 'product_photo', 'category', 'price', 'created_at', 'updated_at')
     success_url = reverse_lazy('catalog:product_list')
-
-
 class ProductUpdateView(UpdateView):
     model = Product
     fields = ('name', 'product_description', 'product_photo', 'category', 'price', 'created_at', 'updated_at')
     success_url = reverse_lazy('catalog:product_list')
-
     def get_success_url(self):
         return reverse('catalog:product_detail', args=[self.kwargs.get('pk')])
-
-
 class ProductDeleteView(DeleteView):
     model = Product
     success_url = reverse_lazy('catalog:product_list')
