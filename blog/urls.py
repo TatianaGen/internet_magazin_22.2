c566c2ae1c783d5c1e1a01b5d0f243d63a9d905c
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-07-31 21:01:45 +0300

    21.2

--- blog/urls.py
+++ blog/urls.py
@@ -1,19 +1,14 @@
 from django.urls import path
 from blog.apps import BlogConfig
-from blog.views import (
-    BlogPostListView,
-    BlogPostDetailView,
-    BlogPostCreateView,
-    BlogPostUpdateView,
-    BlogPostDeleteView,
-)
+from blog.views import (BlogArticleCreateView, BlogArticleListView, BlogArticleDetailView, BlogArticleUpdateView,
+                        BlogArticleDeleteView)
 
 app_name = BlogConfig.name
 
 urlpatterns = [
-    path("", BlogPostListView.as_view(), name="blogpost_list"),
-    path("blog/<int:pk>/", BlogPostDetailView.as_view(), name="blogpost_detail"),
-    path("create/", BlogPostCreateView.as_view(), name="blogpost_form"),
-    path("blog/<int:pk>/update/", BlogPostUpdateView.as_view(), name="blogpost_update"),
-    path("blog/<int:pk>/delete/", BlogPostDeleteView.as_view(), name="blogpost_delete"),
+    path('create/', BlogArticleCreateView.as_view(), name='create'),
+    path('', BlogArticleListView.as_view(), name='list'),
+    path('delete/<int:pk>/', BlogArticleDeleteView.as_view(), name='delete'),
+    path('edit/<int:pk>/', BlogArticleUpdateView.as_view(), name='edit'),
+    path('view/<int:pk>/', BlogArticleDetailView.as_view(), name='view'),
 ]
