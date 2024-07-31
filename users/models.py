5400fc7ae7e5389e8d1481109ce56b73af482694
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-07-31 21:44:36 +0300

    22.2

--- users/models.py
+++ users/models.py
@@ -4,23 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     username = None
-    email = models.EmailField(unique=True, verbose_name="Email")
-    phone = models.CharField(
-        max_length=20,
-        verbose_name="Телефон",
-        blank=True,
-        null=True,
-        help_text="Введите номер телефона",
-    )  # phone number field (библиотека)
-    country = models.CharField(
-        verbose_name="Страна",
-        max_length=20,
-        blank=True,
-        null=True,
-        help_text="Введите страну проживания",
-    )
-    avatar = models.ImageField(upload_to="users/avatars/", blank=True, null=True)
-    token = models.CharField(max_length=100, verbose_name='Token', blank=True, null=True)
+    email = models.EmailField(unique=True, verbose_name='Email')
+    phone = models.CharField(max_length=35, verbose_name='Телефон', blank=True, null=True)
+    country = models.CharField(max_length=35, verbose_name='Страна', blank=True, null=True)
+    avatar = models.ImageField(upload_to='users/avatars/', verbose_name='Аватар', blank=True, null=True)
+    token = models.CharField(max_length=100, verbose_name='Токен', blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
