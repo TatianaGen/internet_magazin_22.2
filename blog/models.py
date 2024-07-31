245b5dba5eab7472a435aec3ad4441cd768f64be
Tatiyana Galitskaya <tatianagalitskaya3@gmail>
2024-07-31 20:57:01 +0300

    21.2

--- blog/models.py
+++ blog/models.py
@@ -1,42 +1,22 @@
 from django.db import models
 
 
-class BlogPost(models.Model):
-    title = models.CharField(
-        max_length=100,
-        verbose_name="Заголовок",
-        help_text="Введите заголовок",
-    )
-    slug = models.SlugField(
-        unique=True,
-        blank=True,
-    )
-    content = models.CharField(
-        max_length=1000,
-        verbose_name="Содержимое",
-        help_text="Введите текст",
-    )
-    preview = models.ImageField(
-        upload_to="BlogPost/photo",
-        blank=True,
-        null=True,
-        verbose_name="Фото",
-        help_text="Загрузите фото"
-    )
-    created_at = models.DateField(
-        verbose_name="Дата создания", auto_now_add=True,
-    )
-    is_published = models.BooleanField(default=True),
-
-    view_count = models.PositiveIntegerField(
-        verbose_name="Счетчик просмотров",
-        help_text="Укажите кол-во просмотров",
-        default=0
-    )
-
-    class Meta:
-        verbose_name = "Блог-пост"
-        verbose_name_plural = "Блог-посты"
+class BlogArticle(models.Model):
+    """
+    Модель для хранения информации о статье блога
+    """
+    title = models.CharField(max_length=100, verbose_name="Заголовок", help_text="Введите заголовок статьи")
+    slug = models.CharField(max_length=150, verbose_name='slug', null=True, blank=True)
+    body = models.TextField(verbose_name="Текст статьи", help_text="Введите текст статьи")
+    preview = models.ImageField(upload_to="products/photo", verbose_name="Изображение",
+                                help_text="Добавьте изображение товара", null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    views_count = models.IntegerField(default=0, verbose_name='Просмотры')
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
